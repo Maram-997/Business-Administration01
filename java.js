139e0ca4767952427a477fac69a7c8fb1adb0ed0
MaramAbu <>
2021-04-14 19:15:35 +0000

    updates

---
+++ java.js
@@ -0,0 +1,11 @@
+var userName = prompt('Welcome, please enter your name.');
+alert('Welcome ' + userName);
+console.log('userName');
+ 
+ var x = prompt('Are you a business administration student? please enter yes or no.');
+ if( x === 'yes')
+{alert ('You Are a FOOL');
+}
+else if( x === 'no' )
+{alert ('You Are SMART');
+}
