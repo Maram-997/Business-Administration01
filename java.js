557746eea4f910c862e40acf784c58908af9be09
MaramAbu <>
2021-04-18 16:02:10 +0000

    NEWUPDATES

--- java.js
+++ java.js
@@ -18,10 +18,27 @@ while (x != 4)
 }
 alert (" Good Job. ");
  
-var y = Number(prompt("How Many Number Of Copies Do You Need?"));
+// var y = Number(prompt("How Many Number Of Copies Do You Need?"));
 
-for (i = 1; i<=y;i++){
+// for (i = 1; i<=y;i++){
  
-var image ='<img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrLfjSlUMy0Si7uwtaU6mApFcLo_ni544YoQ&usqp=CAU " alt =" "/>'
-  document.write(image);
+// var image ='<img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrLfjSlUMy0Si7uwtaU6mApFcLo_ni544YoQ&usqp=CAU " alt =" "/>'
+//   document.write(image);
+// }
+
+
+function askUser (bmissins){
+var ask = prompt("Can you recall the business administration "+ bmissins +"?");
+
+return ask;
 }
+ var ans1 = askUser ("Planning")
+ console.log (ans1);
+ var ans2 = askUser ("Orgnizing")
+ console.log (ans2);
+ var ans3 = askUser ("Coordinating")
+ console.log (ans3);
+ var ans4 = askUser ("Controlling")
+ console.log (ans4);
+
+ alert ("If Any Of The Answers Were No Check The Page Again. :)")
